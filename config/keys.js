3c9e67c85d78844da5e530dfc561f48dad41d518
Tobi kin <44758414+Dxtobi@users.noreply.github.com>
2023-01-16 13:46:58 -0800

    security changes

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI:'mongodb://127.0.0.1:27017/blogerman', //'mongodb+srv://boyxforlife:jummy16snip@nype.asvtv.mongodb.net/blogerman',
+    mongoURI:'mongodb://127.0.0.1:27017/blogerman', //'mongodb+srv://user:password@nype.asvtv.mongodb.net/blogerman',
     secretKey: 'secret'
 };
